7d845e0690db84886c2c576b03b6c79692fede0e
StuffOmarMade <work.omarkhaled@gmail.com>
2023-12-29 16:00:04 +0200

    Squashed commit of the following:
    commit 987be0f40c19d732f5cf985464750882cc5872ca
    Author: Kiroles aziz <73881156+kerolinux@users.noreply.github.com>
    Date:   Fri Dec 29 14:24:38 2023 +0200
    
        update
    
    commit b2728e1d0db058d1d12500e89184f97878829ceb
    Author: Kiroles aziz <73881156+kerolinux@users.noreply.github.com>
    Date:   Fri Dec 29 14:07:01 2023 +0200
    
        fix in menucontroller
    
    commit 4e7ce05915ed0a07b42905900c41687e038e581e
    Author: Kiroles aziz <73881156+kerolinux@users.noreply.github.com>
    Date:   Fri Dec 29 13:55:48 2023 +0200
    
        new update
    
        -wall collision
        -map upgrade.
        -username settings.
        -multiplayer start

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -6,7 +6,7 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadSceneAsync("Lobby");
+        SceneManager.LoadSceneAsync("Multjoin");
     }
     public void QuitGame()
     {
